f027b0c075671016c5a48ad9fa840d5148f671d3
clases clases <clases@labinfo.is.escuelaing.edu.co>
2022-09-06 11:42:33 -0500

    Solucion conflictos

--- PARTE1/src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
+++ PARTE1/src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
@@ -2,11 +2,7 @@ package edu.eci.arsw.springdemo;
 
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
-@Service
-=======
 //@Service
->>>>>>> f5b6c30df07ebb0b3849071b44031aff98fcf4fc
 public class EnglishSpellChecker implements SpellChecker {
 
 	@Override
@@ -14,8 +10,4 @@ public class EnglishSpellChecker implements SpellChecker {
 		return "Checked with english checker:"+text;
 	}
 
-<<<<<<< HEAD
-=======
-        
->>>>>>> f5b6c30df07ebb0b3849071b44031aff98fcf4fc
 }
--- PARTE1/src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
+++ PARTE1/src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
@@ -5,11 +5,8 @@ import org.springframework.stereotype.Service;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-<<<<<<< HEAD
-//@Service
-=======
+
 @Service
->>>>>>> f5b6c30df07ebb0b3849071b44031aff98fcf4fc
 public class SpanishSpellChecker implements SpellChecker {
 
 	@Override
